b77a2904771172168a3fba9f54bc8fcf458dda8c
studio <anchengxing@studios-MacBook-Pro.local>
2016-07-27 21:17:52 +0800

    add remove shortcut function

--- app/src/main/java/com/lolstudio/shortcut/MainActivity.java
+++ app/src/main/java/com/lolstudio/shortcut/MainActivity.java
@@ -3,6 +3,7 @@ package com.lolstudio.shortcut;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +26,20 @@ public class MainActivity extends AppCompatActivity {
         Intent.ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher);
         shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes);
         sendBroadcast(shortcut);
+    }
+
+    /**
+     * http://www.jianshu.com/p/dc3d04337d00
+     * @param v
+     */
+    public void deleteShortCut(View v){
+        Intent actionIntent = new Intent(Intent.ACTION_MAIN);
+        actionIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        actionIntent.setClass(this, SecondActivity.class);
+        Intent intent = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "taotao");
+        intent.putExtra("duplicate", false);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, actionIntent);
+        sendBroadcast(intent);
     }
 }
